52d62fc214ee55a089340dc7dd6e459ab650d9f0
742362144 <742362144@qq.com>
2019-11-13 16:49:50 +0800

    docker

--- docker/vnclet/start.sh
+++ docker/vnclet/start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 cd /home/vnc/bin/noVNC
-nohup ./utils/launch.sh --vnc localhost:5901 > myout.file 2>&1 &
+./utils/launch.sh --vnc localhost:5901
+#nohup ./utils/launch.sh --vnc localhost:5901 > myout.file 2>&1 &
