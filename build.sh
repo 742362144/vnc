d7838a056dda0a497028f2e728b76cccbc9732dd
742362144 <742362144@qq.com>
2019-11-18 11:05:41 +0800

    support docker

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+cd $SHELL_FOLDER
+
+python -m py_compile *.py
+python -m py_compile utils/*.py
+
+cp -rf *.pyc docker/vnclet/
+cp -rf utils/*.pyc docker/vnclet/utils/
+
+cp -rf *.cf docker/vnclet/
+
+cd docker/vnclet
+docker build -t registry.cn-hangzhou.aliyuncs.com/cloudplus-lab/kubeext-vnclet:v1.0
--- docker/vnclet/start.sh
+++ docker/vnclet/start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-cd /home/vnc/bin/noVNC
+python /root/vnc/vnclet.py restart
+
+cd /root/noVNC
 ./utils/launch.sh --vnc localhost:5901
-#nohup ./utils/launch.sh --vnc localhost:5901 > myout.file 2>&1 &
