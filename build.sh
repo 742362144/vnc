65e21bb0b70822f6a87a0ab25f9454895a8b92aa
742362144 <742362144@qq.com>
2019-11-18 11:12:59 +0800

    support docker

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ python -m py_compile utils/*.py
 cp -rf *.pyc docker/vnclet/
 cp -rf utils/*.pyc docker/vnclet/utils/
 
-cp -rf *.cf docker/vnclet/
+cp -rf *.cfg docker/vnclet/
 
 cd docker/vnclet
 docker build -t registry.cn-hangzhou.aliyuncs.com/cloudplus-lab/kubeext-vnclet:v1.0 .
